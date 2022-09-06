cf49cf61c691cf7cac79bbc4b2bd0b778024767e
GHhidatang <wai2333@163.com>
2022-09-06 21:50:56 +0800

    完成日历的整体样式

--- components/CalendarBox/CalendarBox.js
+++ components/CalendarBox/CalendarBox.js
@@ -1,7 +1,10 @@
 // components/CalendarBox/CalendarBox.js
-var util = require('../../utils/util.js')
+// var util = require('../../utils/util.js')
+// component.js
+const computedBehavior = require('miniprogram-computed').behavior
 
 Component({
+  behaviors: [computedBehavior],
   /**
    * 组件的属性列表
    */
@@ -13,9 +16,21 @@ Component({
    */
   data: {
     show: false,
-    month: util.formatTime(new Date()),
-    years: new Date().getFullYear(),
-    months: new Date().getMonth() + 1
+    nowYear: new Date().getFullYear(),
+    months: new Date().getMonth() + 1,
+    month_s: 12,
+    day: new Date().getDate(),
+    year: new Date().getFullYear(),
+    selectMonth: new Date().getMonth() + 1
+  },
+  computed: {
+    getMonths(data) {
+      if (data.year == data.nowYear) {
+        return new Date().getMonth() + 1
+      }else {
+        return +12
+      }
+    }
   },
 
   /**
@@ -34,5 +49,26 @@ Component({
         show: true
       })
     },
+    // 弹出层上一年
+    lastYear() {
+      this.setData({
+        year: this.data.year - 1
+      })
+    },
+    // 弹出层下一年
+    nextYear() {
+      if (this.data.year != this.data.nowYear) {
+        this.setData({
+          year: this.data.year + 1
+        })
+      }
+    },
+    // 点击选择月份
+    clickOnSelectMonth(e) {
+      this.setData({
+        selectMonth: e.currentTarget.dataset.month + 1,
+        show: false
+      })
+    }
   }
 })
--- components/calendar/calendar.js
+++ components/calendar/calendar.js
@@ -10,12 +10,30 @@ Component({
    * 组件的初始数据
    */
   data: {
+    day_date: ['星期天', '星期一', '星期二', '星期三', '星期四', '星期五', '星期六'],
+    lastDay: new Date(new Date().getFullYear(), new Date().getMonth() + 1, 0).getDate(),
+    week: new Date(new Date().getFullYear() + new Date().getMonth() + 1 + 1).getDay(),
+    day: new Date().getDate()
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    
+    calendarDays(e) {
+      console.log(e);
+      this.setData({
+        day: e.currentTarget.dataset.mothnum + 1
+      });
+      console.log(this.data.day);
+    }
+  },
+
+  lifetimes: {
+    // 组件初始化完毕 进入页面节点树后触发
+    // attached: function() {
+    //   console.log(this.data.lastDay);
+    //   console.log(this.data.week);
+    // }
   }
 })
